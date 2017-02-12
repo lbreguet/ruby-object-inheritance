b3188e71e4d05c7e3e55e284f9915918a7f5a39d
lbreguet <louis-w@live.com>
2017-02-11 19:32:58 -0500

    Ruby inheritance intro
    -review inheritance methods

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -1,3 +1,20 @@
 # frozen_string_literal: true
 
 require_relative 'shape'
+
+class Rectangle < Shape
+  attr_reader :width, :height
+  def initialize(width, height)
+    @width = width
+    @height = height
+    @num_sides = 4
+    @side_length = [height, width]
+  end
+
+  def calculate_area
+    width * height
+  end
+end
+
+box = Rectangle.new(3, 4)
+p box.calculate_area
--- lib/square.rb
+++ lib/square.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 
 require_relative 'rectangle'
+
+class Square < Rectangle
+  def initialize(length)
+    super(length, length)
+  end
+end
+
+bob = Square.new(4)
+p bob.calculate_area
